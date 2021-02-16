ed01d4e32975f978a3c1652888fd46d58427a7a6
Hipri Joven <fatima.hiprenoridel@gmail.com>
2021-02-16 11:53:50 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Your Name Here</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-default">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="index.html">Fatima Hiprenoridel</a>
+            </div>
+        
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="personal_statement.html">Personal Statement</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+
+    <div class="container">
+        <!-- Summary -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"><strong>Your Title</strong></h3>
+            </div>
+            <div class="panel-body">
+                <div class="col-md-8">
+                    <strong>Job 1</strong>
+                    <p align="justify">Description of Job</p>
+                </div>
+                <div class="col-md-4" align="right"><em>2000 - 2021</em></div>
+            </div>
+        </div>
+        <br /><br />
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"><strong>Programming Technical Skills</strong></h3>
+            </div>
+            <div class="panel-body">
+                <div class="col-md-3">Stuff</div>
+                <div class="col-md-3">Stuff</div>
+                <div class="col-md-3">Stuff</div>
+                <div class="col-md-3">Stuff</div>
+            </div>
+        </div>
+        <br /><br />
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"><strong>Education</strong></h3>
+            </div>
+            <div class="panel-body">
+                <div class="col-md-1">
+                    <img src="/images/somejpg.jpg">
+                </div>
+                <div class="col-md-4">
+                    <strong>School Name</strong><br /></br>
+                    <em>Course</em>
+                </div>
+            </div>
+        </div>
+        <br /><br />
+        <p align="center">&copy; Fatima Hiprenoridel Joven - All Rights Reserved</p>
+    </div>
+</body>
+
+</html>
